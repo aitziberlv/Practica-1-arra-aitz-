2ba47fc5315eb16d6ffb2dac0d0305f672e56f7c
arrat <arrat@Arrate_Nogales>
2022-10-02 21:34:41 +0200

    commit

---
+++ Practica1_arra aitz/Test/ud/prog3/pr0/ListaDeReproduccionTest.java
@@ -0,0 +1,15 @@
+package ud.prog3.pr0;
+
+import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ListaDeReproduccionTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
